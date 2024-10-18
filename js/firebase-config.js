91141001b5d993f0616a3cc64a1f89d80655c190
Eduardo Canelas Eterovic <153656768+eduardo-canelas@users.noreply.github.com>
2024-10-18 17:52:45 -0400

    Update firebase-config.js
    firebase config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,9 +4,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-analytics.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
-import {
-  getFirestore, collection
-} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -24,8 +21,5 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
-const db = getFirestore()
-
-const colRef = collection(db, 'habits')
 // Export the auth instance for use in other modules
 export { auth };
